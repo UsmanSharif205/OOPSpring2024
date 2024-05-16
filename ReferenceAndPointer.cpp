d3d96f2f11d30da2c52efe71ada8705e7a60e64b
UsmanSharif205 <168710163+UsmanSharif205@users.noreply.github.com>
2024-05-16 12:37:04 +0500

    Create Call by Reference And Value

---
+++ ReferenceAndPointer.cpp
@@ -0,0 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    /*
+    int a =5;
+    int *ptr = &a;
+    
+    cout<<a<<endl;
+    cout<<&a<<endl;
+    cout<<endl;
+    cout<<ptr<<endl;
+    cout<<&ptr<<endl;
+    cout<<*ptr<<endl;
+*/
+int a[5] = {1,2,3,4,5};
+cout<<a<<endl;
+cout<<&a[0]<<endl;
+cout<<&a[1];
+    return 0;
+}
